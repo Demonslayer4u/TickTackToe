7234154e626035a7143aa6b7a411770add55cddc
Demonslayer4u <fabispaceman007@gmail.com>
2025-05-18 21:27:27 +0200

    TickTackToe-1.1

--- ticktacktoe.py
+++ ticktacktoe.py
@@ -8,10 +8,20 @@ B  {board_raw[3]} | {board_raw[4]} | {board_raw[5]}
 C  {board_raw[6]} | {board_raw[7]} | {board_raw[8]}
 """
 
-SpalteInput = None
-ZeileInput = None
+split_input = None
+line_input = None
+
+input0 = None
+input1 = None
+
+input0_is_int = False
+input1_is_int = False
+
+player_turn = "X"
+
 
 def get_board_con():
+    #print("Board updated...")
     return f"""   1   2   3
 A  {board_raw[0]} | {board_raw[1]} | {board_raw[2]}
   ---+---+---
@@ -20,15 +30,34 @@ B  {board_raw[3]} | {board_raw[4]} | {board_raw[5]}
 C  {board_raw[6]} | {board_raw[7]} | {board_raw[8]}
 """
 
+#reset
+def reset_board():
+    board_raw[0] = " "
+    board_raw[1] = " "
+    board_raw[2] = " "
+    board_raw[3] = " "
+    board_raw[4] = " "
+    board_raw[5] = " "
+    board_raw[6] = " "
+    board_raw[7] = " "
+    board_raw[8] = " "
+    #print("Board reset..")
+    user_input()
+
 
 #start
 def start():
+    global player_turn
+    reset_board()
+    player_turn = "X"
     print("TickTackToe")
-    turnP1()
+    #print("Start...")
+
 
 
 #menu
 def menu():
+    #print("menu...")
     exityn = input("Play again (y/n)?")
     if exityn == "y":
         start()
@@ -36,239 +65,195 @@ def menu():
         exit()
 
 
-#p1
-def turnP1():
-    print(" \nSpieler 1 (X) ist dran!")
+
+#input
+def user_input():
+    #print("User_input...")
+
+    global player_turn
+    print(f" \nSpieler {player_turn} ist dran!")
     print(get_board_con())
 
-
-def Input():
-    input0 = input("Eingabe...")
-    input1 = input0[1]
-
-
-
-def inputSetzenP1():
-    global SpalteInput
-    global ZeileInput
-
-    if SpalteInput == "1":
-        if ZeileInput in ("A", "a"):
-            if board_raw[0] == " ":
-                board_raw[0] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
-        elif ZeileInput in ("B", "b"):
-            if board_raw[3] == " ":
-                board_raw[3] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
-        elif ZeileInput in ("C", "c"):
-            if board_raw[6] == " ":
-                board_raw[6] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
-    elif SpalteInput == "2":
-        if ZeileInput in ("A", "a"):
-            if board_raw[1] == " ":
-                board_raw[1] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
-        elif ZeileInput in ("B", "b"):
-            if board_raw[4] == " ":
-                board_raw[4] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
-        elif ZeileInput in ("C", "c"):
-            if board_raw[7] == " ":
-                board_raw[7] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
-    elif SpalteInput == "3":
-        if ZeileInput in ("A", "a"):
-            if board_raw[2] == " ":
-                board_raw[2] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
-        elif ZeileInput in ("B", "b"):
-            if board_raw[5] == " ":
-                board_raw[5] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
-        elif ZeileInput in ("C", "c"):
-            if board_raw[8] == " ":
-                board_raw[8] = "X"
-                gewinnerkennungP1()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP1()
+    global input0
+    global input1
+    input_raw = input(f"Setze dein {player_turn} \n")
+    if len(input_raw) == 2:
+        input0 = input_raw[0]
+        input1 = input_raw[1]
+        conversion1()
     else:
-        print("Error 1")
-        menu()
+        print("Ungültige Eingabe")
+        user_input()
 
-def gewinnerkennungP1():
+def conversion1():
+    global input0
+    global input0_is_int
+    #print("Conversion1...")
+    try:
+        input0 = int(input0)
+        input0_is_int = True
+        conversion2()
+    except ValueError:
+        input0 = str(input0)
+        input0_is_int = False
+        conversion2()
+
+def conversion2():
+    global input1
+    global input1_ist_int
+    #print("Conversion2...")
+    try:
+        input1 = int(input1)
+        input1_ist_int = True
+        checking_input()
+    except ValueError:
+        input1 = str(input1)
+        input1_ist_int = False
+        checking_input()
+
+def checking_input():
+    global input0_ist_int
+    global input0_ist_int
+    global split_input
+    global line_input
+    #print("Checking_input...")
+    #print(Player_turn)
+
+    if input0_ist_int == input1_ist_int:
+        print("Ungültige Eingabe")
+        user_input()
+    else:
+        if input0_ist_int:
+            split_input = str(input0)
+            line_input = str(input1)
+            #print("Inp0 is int...")
+            placing()
+        elif input1_ist_int:
+            #print("Inp1 is int...")
+            split_input = str(input1)
+            line_input = str(input0)
+            placing()
+        else:
+            print("Error: Überprüfung")
+            menu()
+
+
+
+#input placen
+def placing():
+    global split_input
+    global line_input
+    #print("split: ",split_input)
+    #print("line: ",line_input)
+
+    if split_input == "1":
+        if line_input in ("A", "a"):
+            if board_raw[0] == " ":
+                board_raw[0] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+                user_input()
+        elif line_input in ("B", "b"):
+            if board_raw[3] == " ":
+                board_raw[3] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+                user_input()
+        elif line_input in ("C", "c"):
+            if board_raw[6] == " ":
+                board_raw[6] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+                user_input()
+    elif split_input == "2":
+        if line_input in ("A", "a"):
+            if board_raw[1] == " ":
+                board_raw[1] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+                user_input()
+        elif line_input in ("B", "b"):
+            if board_raw[4] == " ":
+                board_raw[4] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+                user_input()
+        elif line_input in ("C", "c"):
+            if board_raw[7] == " ":
+                board_raw[7] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+                user_input()
+    elif split_input == "3":
+        if line_input in ("A", "a"):
+            if board_raw[2] == " ":
+                board_raw[2] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+                user_input()
+        elif line_input in ("B", "b"):
+            if board_raw[5] == " ":
+                board_raw[5] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+            user_input()
+        elif line_input in ("C", "c"):
+            if board_raw[8] == " ":
+                board_raw[8] = f"{player_turn}"
+                checking_winner()
+            else:
+                print("Feld ist schon belegt, bitte neue user_input!")
+                user_input()
+    else:
+        print("Ungültige Eingabe")
+        user_input()
+
+def checking_winner():
     global board_raw
-    if board_raw[0] == "X" and board_raw[1] == "X" and board_raw[2] == "X":
-        winnerP1()
-    elif board_raw[3] == "X" and board_raw[4] == "X" and board_raw[5] == "X":
-        winnerP1()
-    elif board_raw[6] == "X" and board_raw[7] == "X" and board_raw[8] == "X":
-        winnerP1()
-    elif board_raw[0] == "X" and board_raw[3] == "X" and board_raw[6] == "X":
-        winnerP1()
-    elif board_raw[1] == "X" and board_raw[4] == "X" and board_raw[7] == "X":
-        winnerP1()
-    elif board_raw[2] == "X" and board_raw[5] == "X" and board_raw[8] == "X":
-        winnerP1()
-    elif board_raw[0] == "X" and board_raw[4] == "X" and board_raw[8] == "X":
-        winnerP1()
-    elif board_raw[2] == "X" and board_raw[4] == "X" and board_raw[6] == "X":
-        winnerP1()
+    global player_turn
+    #print("Checking_winner...")
+    if board_raw[0] == f"{player_turn}" and board_raw[1] == f"{player_turn}" and board_raw[2] == f"{player_turn}":
+        winner()
+    elif board_raw[3] == f"{player_turn}" and board_raw[4] == f"{player_turn}" and board_raw[5] == f"{player_turn}":
+        winner()
+    elif board_raw[6] == f"{player_turn}" and board_raw[7] == f"{player_turn}" and board_raw[8] == f"{player_turn}":
+        winner()
+    elif board_raw[0] == f"{player_turn}" and board_raw[3] == f"{player_turn}" and board_raw[6] == f"{player_turn}":
+        winner()
+    elif board_raw[1] == f"{player_turn}" and board_raw[4] == f"{player_turn}" and board_raw[7] == f"{player_turn}":
+        winner()
+    elif board_raw[2] == f"{player_turn}" and board_raw[5] == f"{player_turn}" and board_raw[8] == f"{player_turn}":
+        winner()
+    elif board_raw[0] == f"{player_turn}" and board_raw[4] == f"{player_turn}" and board_raw[8] == f"{player_turn}":
+        winner()
+    elif board_raw[2] == f"{player_turn}" and board_raw[4] == f"{player_turn}" and board_raw[6] == f"{player_turn}":
+        winner()
     elif " " not in board_raw:
         print("Unentschieden!")
         print(get_board_con())
         menu()
-    else: turnP2()
-
-def winnerP1():
-    print("Glückwunsch Spieler 1, du hast gewonnen!")
-    print(get_board_con())
-    menu()
-
-#p2
-def turnP2():
-    print(" \nSpieler 2 (O) ist dran!")
-    print(get_board_con())
-    spaltenInputP2()
-
-def spaltenInputP2():
-    global SpalteInput
-    SpalteInput = input("Wo setzt du deinen O? (Spalte)")
-    if SpalteInput in ("1", "2", "3"):
-        zeilenInputP2()
-    else: spaltenInputP2()
-
-def zeilenInputP2():
-    global ZeileInput
-    ZeileInput = input("Wo setzt du deinen O? (Zeile)")
-    if ZeileInput in ("A", "B", "C", "a", "b", "c"):
-        inputSetzenP2()
-    else: zeilenInputP2()
-
-def inputSetzenP2():
-    global SpalteInput
-    global ZeileInput
-
-    if SpalteInput == "1":
-        if ZeileInput in ("A", "a"):
-            if board_raw[0] == " ":
-                board_raw[0] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
-        elif ZeileInput in ("B", "b"):
-            if board_raw[3] == " ":
-                board_raw[3] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
-        elif ZeileInput in ("C", "c"):
-            if board_raw[6] == " ":
-                board_raw[6] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
-    elif SpalteInput == "2":
-        if ZeileInput in ("A", "a"):
-            if board_raw[1] == " ":
-                board_raw[1] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
-        elif ZeileInput in ("B", "b"):
-            if board_raw[4] == " ":
-                board_raw[4] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
-        elif ZeileInput in ("C", "c"):
-            if board_raw[7] == " ":
-                board_raw[7] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
-    elif SpalteInput == "3":
-        if ZeileInput in ("A", "a"):
-            if board_raw[2] == " ":
-                board_raw[2] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
-        elif ZeileInput in ("B", "b"):
-            if board_raw[5] == " ":
-                board_raw[5] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
-        elif ZeileInput in ("C", "c"):
-            if board_raw[8] == " ":
-                board_raw[8] = "O"
-                gewinnerkennungP2()
-            else:
-                print("Feld ist schon belegt, bitte neue Eingabe!")
-                spaltenInputP2()
     else:
-        print("Error 2")
-        menu()
+        if player_turn == "X":
+            player_turn = "O"
+            user_input()
+        else:
+            player_turn = "X"
+            user_input()
 
-def gewinnerkennungP2():
-    global board_raw
-    if board_raw[0] == "X" and board_raw[1] == "X" and board_raw[2] == "X":
-        winnerP2()
-    elif board_raw[3] == "X" and board_raw[4] == "X" and board_raw[5] == "X":
-        winnerP2()
-    elif board_raw[6] == "X" and board_raw[7] == "X" and board_raw[8] == "X":
-        winnerP2()
-    elif board_raw[0] == "X" and board_raw[3] == "X" and board_raw[6] == "X":
-        winnerP2()
-    elif board_raw[1] == "X" and board_raw[4] == "X" and board_raw[7] == "X":
-        winnerP2()
-    elif board_raw[2] == "X" and board_raw[5] == "X" and board_raw[8] == "X":
-        winnerP2()
-    elif board_raw[0] == "X" and board_raw[4] == "X" and board_raw[8] == "X":
-        winnerP2()
-    elif board_raw[2] == "X" and board_raw[4] == "X" and board_raw[6] == "X":
-        winnerP2()
-    else: turnP1()
-
-def winnerP2():
-    print("Glückwunsch Spieler 2, du hast gewonnen!")
+def winner():
+    print(f"Glückwunsch Spieler {player_turn}, du hast gewonnen!")
     print(get_board_con())
     menu()
+
+
 
 
 start()
